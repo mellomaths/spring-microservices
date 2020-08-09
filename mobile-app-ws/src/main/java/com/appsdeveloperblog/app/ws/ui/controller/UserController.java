a31c2e9e6f23b930c1fbcc325baf3a033643b94f
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-08-09 19:41:39 -0300

    Add content type produced xml and json

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.appsdeveloperblog.app.ws.ui.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,30 +16,30 @@ import com.appsdeveloperblog.app.ws.ui.model.response.UserRest;
 @RequestMapping("users")
 public class UserController {
 	
-	@GetMapping
+	@GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 	public String getUsers(@RequestParam(value = "page", defaultValue="1") int page, 
 			@RequestParam(value = "limit", defaultValue = "100") int limit,
 			@RequestParam(value = "sort", defaultValue = "desc", required = false) String sort) {
 		return "GET /users was called! Page = " + page + " and Limit = " + limit;
 	}
 	
-	@PostMapping
+	@PostMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 	public String createUser() {
 		return "POST /users was called!";
 	}
 	
-	@GetMapping(path = "/{userId}")
+	@GetMapping(path = "/{userId}", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 	public UserRest getUserById(@PathVariable String userId) {
 		UserRest returnValue = new UserRest(userId, "Matheus", "Mello de Lima", "mellomatheuslima@gmail.com");
 		return returnValue;
 	}
 	
-	@PutMapping(path = "/{userId}")
+	@PutMapping(path = "/{userId}", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 	public String updateUser(@PathVariable String userId) {
 		return "PUT /users/" + userId + "  was called!";
 	}
 	
-	@DeleteMapping(path = "/{userId}")
+	@DeleteMapping(path = "/{userId}", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 	public String deleteUser(@PathVariable String userId) {
 		return "DELETE /users/" + userId + "  was called!";
 	}
