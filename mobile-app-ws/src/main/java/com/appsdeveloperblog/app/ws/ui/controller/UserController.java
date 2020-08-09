31cdb5a9ababf0f4f665a0e78fde557dedf4a5ea
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-08-09 14:51:46 -0300

    Add Rest Controller for user

---
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.appsdeveloperblog.app.ws.ui.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+	
+	
+}
