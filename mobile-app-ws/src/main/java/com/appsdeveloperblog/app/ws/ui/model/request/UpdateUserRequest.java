b9117c6e907c24c38a2578157f007f59e7082124
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-08-10 23:33:48 -0300

    Create update user request model

---
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/ui/model/request/UpdateUserRequest.java
@@ -0,0 +1,37 @@
+package com.appsdeveloperblog.app.ws.ui.model.request;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+public class UpdateUserRequest {
+	
+	@NotNull(message = "Field 'firstName' is mandatory.")
+	@NotBlank(message = "Field 'firstName' is mandatory.")
+	private String firstName;
+	
+	@NotNull(message = "Field 'lastName' is mandatory.")
+	@NotBlank(message = "Field 'lastName' is mandatory.")
+	private String lastName;
+	
+	public UpdateUserRequest(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
