0b9007f781166dbcf5f93f67f6253192385bf8c3
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-08-15 13:31:26 -0300

    Add Playlist document

---
+++ webflux/src/main/java/com/apirest/webflux/document/Playlist.java
@@ -0,0 +1,37 @@
+package com.apirest.webflux.document;
+
+import java.util.UUID;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Playlist {
+	
+	@Id
+	private String id;
+	private String name;
+	
+	public Playlist(String name) {
+		this.id = UUID.randomUUID().toString();
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
