39888226be85280163d469eed915de899bd6a128
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-08-22 14:58:08 -0300

    Start Eureka Service Discovery

---
+++ PhotoAppDiscoveryService/src/test/java/com/spring/microservices/photoapp/discovery/PhotoAppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.microservices.photoapp.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
