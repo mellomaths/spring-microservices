4efe0ea01a3544c7b3737e0648dc70264b669e6e
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-08-23 22:30:59 -0300

    Start spring project for users microservice

---
+++ PhotoAppApiUsers/src/test/java/com/spring/microservices/photoapp/api/users/PhotoAppApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.microservices.photoapp.api.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
