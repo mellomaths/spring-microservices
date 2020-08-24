17d98236e7e18f21c0456a753c5e3dc20f769e65
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-08-23 22:55:01 -0300

    Enable Eureka Discovery Client

--- PhotoAppApiAccountManagement/src/main/java/com/spring/microservices/photoapp/api/account/PhotoAppApiAccountManagementApplication.java
+++ PhotoAppApiAccountManagement/src/main/java/com/spring/microservices/photoapp/api/account/PhotoAppApiAccountManagementApplication.java
@@ -2,8 +2,10 @@ package com.spring.microservices.photoapp.api.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PhotoAppApiAccountManagementApplication {
 
 	public static void main(String[] args) {
