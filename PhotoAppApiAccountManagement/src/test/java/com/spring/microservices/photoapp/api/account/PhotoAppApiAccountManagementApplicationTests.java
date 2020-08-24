d99d464825c2fd03974598069fa6ec1ad7df74fc
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-08-23 22:54:01 -0300

    Start spring project for account management microservice

---
+++ PhotoAppApiAccountManagement/src/test/java/com/spring/microservices/photoapp/api/account/PhotoAppApiAccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.microservices.photoapp.api.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiAccountManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
