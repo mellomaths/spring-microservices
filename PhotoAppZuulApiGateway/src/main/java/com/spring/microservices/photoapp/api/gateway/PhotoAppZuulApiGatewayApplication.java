114c7061f489753a88307881cd83c25a1a607afe
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-08-24 22:51:08 -0300

    Add Zuul API Gateway spring project

---
+++ PhotoAppZuulApiGateway/src/main/java/com/spring/microservices/photoapp/api/gateway/PhotoAppZuulApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.spring.microservices.photoapp.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppZuulApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppZuulApiGatewayApplication.class, args);
+	}
+
+}
