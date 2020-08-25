0da705f339254ab1ae6f6c2181f1036e58c87830
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-08-24 23:24:44 -0300

    Show port on health check to differenciate the instances on load balancer

--- PhotoAppApiUsers/src/main/java/com/spring/microservices/photoapp/api/users/application/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/spring/microservices/photoapp/api/users/application/controllers/UsersController.java
@@ -1,5 +1,7 @@
 package com.spring.microservices.photoapp.api.users.application.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("users")
 public class UsersController {
 	
+	@Autowired
+	private Environment env;
+	
 	@GetMapping("health")
 	public String healthCheck() {
-		return "Working!";
+		return "Working on port " + env.getProperty("local.server.port");
 	}
 	
 }
