02374f69a799f30e6ccdff90da8363fff856cf24
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-09-01 18:07:45 -0300

    Add basic structure for implementing user service

---
+++ PhotoAppApiUsers/src/main/java/com/spring/microservices/photoapp/api/users/domain/service/UsersServiceImpl.java
@@ -0,0 +1,13 @@
+package com.spring.microservices.photoapp.api.users.domain.service;
+
+import com.spring.microservices.photoapp.api.users.shared.UserDto;
+
+public class UsersServiceImpl implements UsersService {
+
+	@Override
+	public void createUser(UserDto userData) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
