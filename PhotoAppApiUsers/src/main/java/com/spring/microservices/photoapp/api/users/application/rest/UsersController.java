f7652e61be7c4ae004b5f929d30a8ccc5e6c85dd
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-09-01 16:14:44 -0300

    Handle POST user request with request body

--- PhotoAppApiUsers/src/main/java/com/spring/microservices/photoapp/api/users/application/rest/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/spring/microservices/photoapp/api/users/application/rest/UsersController.java
@@ -1,10 +1,14 @@
-package com.spring.microservices.photoapp.api.users.application.controllers;
+package com.spring.microservices.photoapp.api.users.application.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.microservices.photoapp.api.users.application.request.CreateUserRequest;
 
 @RestController
 @RequestMapping("users")
@@ -16,6 +20,11 @@ public class UsersController {
 	@GetMapping("health")
 	public String healthCheck() {
 		return "Working on port " + env.getProperty("local.server.port");
+	}
+	
+	@PostMapping
+	public String createUser(@RequestBody CreateUserRequest userData) {
+		return "createUser() method was called!";
 	}
 	
 }
