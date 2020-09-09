448a223c13f86ee8d84c1f9378347b50f8afbc48
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-09-08 21:06:10 -0300

    Change User reference to new user entity

--- PhotoAppApiUsers/src/main/java/com/spring/microservices/photoapp/api/users/domain/service/UsersServiceImpl.java
+++ PhotoAppApiUsers/src/main/java/com/spring/microservices/photoapp/api/users/domain/service/UsersServiceImpl.java
@@ -1,5 +1,6 @@
 package com.spring.microservices.photoapp.api.users.domain.service;
 
+import com.spring.microservices.photoapp.api.users.domain.User;
 import com.spring.microservices.photoapp.api.users.shared.UserDto;
 
 public class UsersServiceImpl implements UsersService {
@@ -7,6 +8,7 @@ public class UsersServiceImpl implements UsersService {
 	@Override
 	public void createUser(UserDto userData) {
 		// TODO Auto-generated method stub
+		User user = User.of(userData);
 		
 	}
 	
