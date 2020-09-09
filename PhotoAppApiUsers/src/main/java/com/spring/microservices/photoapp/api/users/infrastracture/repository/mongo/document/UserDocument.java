a55144fc819c435598eea31632e3ffba7f0b6687
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-09-08 21:23:39 -0300

    Add mongo user document

---
+++ PhotoAppApiUsers/src/main/java/com/spring/microservices/photoapp/api/users/infrastracture/repository/mongo/document/UserDocument.java
@@ -0,0 +1,43 @@
+package com.spring.microservices.photoapp.api.users.infrastracture.repository.mongo.document;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.spring.microservices.photoapp.api.users.infrastracture.entity.UserEntity;
+import com.spring.microservices.photoapp.api.users.shared.UserDto;
+
+@Document(collection = "user")
+public class UserDocument extends UserEntity {
+	
+	@Id
+	private String id;
+	
+	public UserDocument(String id, String firstName, String lastName, String email, String encryptedPassword) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.encryptedPassword = encryptedPassword;
+	}
+	
+	public static UserDocument of(UserDto userData) {
+		UserDocument document = new UserDocument(
+				userData.getId(), 
+				userData.getFirstName(), 
+				userData.getLastName(), 
+				userData.getEmail(),
+				userData.getEncryptedPassword());
+		return document;
+	}
+	
+	public UserDto toDto() {
+		UserDto dto = new UserDto();
+		dto.setId(id.toString());
+		dto.setFirstName(firstName);
+		dto.setLastName(lastName);
+		dto.setEmail(email);
+		dto.setEncryptedPassword(encryptedPassword);
+		return dto;
+	}
+
+}
