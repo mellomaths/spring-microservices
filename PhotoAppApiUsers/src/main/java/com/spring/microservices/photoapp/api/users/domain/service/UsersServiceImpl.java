b65c6e27f84edc81ec9b7eeaf75d316e50eca02e
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-09-09 16:21:04 -0300

    Add user save impl

--- PhotoAppApiUsers/src/main/java/com/spring/microservices/photoapp/api/users/domain/service/UsersServiceImpl.java
+++ PhotoAppApiUsers/src/main/java/com/spring/microservices/photoapp/api/users/domain/service/UsersServiceImpl.java
@@ -1,15 +1,26 @@
 package com.spring.microservices.photoapp.api.users.domain.service;
 
+import org.springframework.stereotype.Service;
+
 import com.spring.microservices.photoapp.api.users.domain.User;
+import com.spring.microservices.photoapp.api.users.domain.repository.UserRepository;
 import com.spring.microservices.photoapp.api.users.shared.UserDto;
 
+@Service
 public class UsersServiceImpl implements UsersService {
+	
+	private final UserRepository userRepository;
+	
+	public UsersServiceImpl(final UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
 
 	@Override
-	public void createUser(UserDto userData) {
+	public String createUser(UserDto userData) {
 		// TODO Auto-generated method stub
 		User user = User.of(userData);
-		
+		userRepository.save(user.toDto());
+		return user.getId().toString();
 	}
 	
 }
