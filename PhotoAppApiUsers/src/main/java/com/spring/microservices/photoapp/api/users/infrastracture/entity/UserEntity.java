27305af6d6d000277e864955e7c7090300866264
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-09-08 21:23:25 -0300

    Add user entity on infra package

---
+++ PhotoAppApiUsers/src/main/java/com/spring/microservices/photoapp/api/users/infrastracture/entity/UserEntity.java
@@ -0,0 +1,11 @@
+package com.spring.microservices.photoapp.api.users.infrastracture.entity;
+
+public class UserEntity {
+	
+	protected String id;
+	protected String firstName;
+	protected String lastName;
+	protected String email;
+	protected String encryptedPassword;
+
+}
