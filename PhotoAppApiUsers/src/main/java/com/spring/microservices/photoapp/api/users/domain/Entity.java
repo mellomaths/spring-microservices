bbd2de1c4e2c137a612ce6ecbb09e94db56a2658
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-09-10 11:09:37 -0300

    Use created at date on entities

--- PhotoAppApiUsers/src/main/java/com/spring/microservices/photoapp/api/users/domain/Entity.java
+++ PhotoAppApiUsers/src/main/java/com/spring/microservices/photoapp/api/users/domain/Entity.java
@@ -1,13 +1,16 @@
 package com.spring.microservices.photoapp.api.users.domain;
 
+import com.spring.microservices.photoapp.api.users.domain.valueobjects.Date;
 import com.spring.microservices.photoapp.api.users.domain.valueobjects.Identifier;
 
 public class Entity {
 	
 	Identifier id;
+	Date createdAt;
 	
 	public Entity() {
 		this.id = new Identifier();
+		this.createdAt = new Date();
 	}
 
 }
