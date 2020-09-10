4e3747d941c8200ab8b2cdfe0c626bd0453ea2d3
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-09-10 16:34:31 -0300

    Add user repository method to find user by id

--- PhotoAppApiUsers/src/main/java/com/spring/microservices/photoapp/api/users/domain/repository/UserRepository.java
+++ PhotoAppApiUsers/src/main/java/com/spring/microservices/photoapp/api/users/domain/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.spring.microservices.photoapp.api.users.domain.repository;
 
+import com.spring.microservices.photoapp.api.users.domain.exception.UserNotFoundException;
 import com.spring.microservices.photoapp.api.users.shared.UserDto;
 
 public interface UserRepository {
 	
 	void save(UserDto userData);
+	
+	UserDto findById(String id) throws UserNotFoundException; 
 
 }
