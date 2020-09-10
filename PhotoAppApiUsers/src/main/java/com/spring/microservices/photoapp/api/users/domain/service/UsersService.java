9f9b43bcf8d38ba1ae0ec2b1cd9a0329aba29378
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-09-10 16:35:15 -0300

    Add user get by id method on service

--- PhotoAppApiUsers/src/main/java/com/spring/microservices/photoapp/api/users/domain/service/UsersService.java
+++ PhotoAppApiUsers/src/main/java/com/spring/microservices/photoapp/api/users/domain/service/UsersService.java
@@ -1,8 +1,11 @@
 package com.spring.microservices.photoapp.api.users.domain.service;
 
+import com.spring.microservices.photoapp.api.users.domain.exception.UserNotFoundException;
 import com.spring.microservices.photoapp.api.users.shared.UserDto;
 
 public interface UsersService {
 
 	String createUser(UserDto userData);
+	
+	UserDto getUserById(String id) throws UserNotFoundException;
 }
