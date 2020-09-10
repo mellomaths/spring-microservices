8bccf41f66e52dbe9f073669b61462f50218bd87
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-09-10 16:33:58 -0300

    Add user not found exception

---
+++ PhotoAppApiUsers/src/main/java/com/spring/microservices/photoapp/api/users/domain/exception/UserNotFoundException.java
@@ -0,0 +1,18 @@
+package com.spring.microservices.photoapp.api.users.domain.exception;
+
+public class UserNotFoundException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String userId;
+	
+	public UserNotFoundException(String userId) {
+		super("User " + userId + " was not found.");
+		this.userId = userId;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+	
+}
