11f6d927b2872100196559865d96032c27d1393a
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-09-12 19:29:11 -0300

    Configure the user microservice to be visible only for api gateway

--- PhotoAppApiUsers/src/main/java/com/spring/microservices/photoapp/api/users/application/http/configuration/Security.java
+++ PhotoAppApiUsers/src/main/java/com/spring/microservices/photoapp/api/users/application/http/configuration/Security.java
@@ -1,6 +1,8 @@
 package com.spring.microservices.photoapp.api.users.application.http.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,10 +11,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class Security extends WebSecurityConfigurerAdapter {
 	
+	private final Environment environment;
+	
+	@Autowired
+	public Security(final Environment environment) {
+		this.environment = environment; 
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
-		http.authorizeRequests().antMatchers("/users/**").permitAll();
+		http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("apigateway.ip.address"));
 	}
  
 }
