d8f14fee8f17fc93a41c4650b7bdfaf743d940f0
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-11-09 21:31:29 -0300

    Permit requests to registration and login without the need to be authenticated

--- PhotoAppZuulApiGateway/src/main/java/com/spring/microservices/photoapp/api/gateway/security/Security.java
+++ PhotoAppZuulApiGateway/src/main/java/com/spring/microservices/photoapp/api/gateway/security/Security.java
@@ -3,6 +3,7 @@ package com.spring.microservices.photoapp.api.gateway.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -23,6 +24,10 @@ public class Security extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.headers().frameOptions().disable();
+		
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.POST, env.getRequiredProperty("api.registration.url.path")).permitAll()
+			.antMatchers(HttpMethod.POST, env.getRequiredProperty("api.login.url.path")).permitAll();
 		
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		// Make the API stateless
