e8cacbf79241b008643e3731b734205502f360ba
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-11-16 14:25:53 -0300

    Update test to respect new version

--- PhotoAppApiConfigServer/src/test/java/com/spring/microservices/photoapp/api/PhotoAppApiConfigServerApplicationTests.java
+++ PhotoAppApiConfigServer/src/test/java/com/spring/microservices/photoapp/api/PhotoAppApiConfigServerApplicationTests.java
@@ -1,6 +1,6 @@
 package com.spring.microservices.photoapp.api;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
