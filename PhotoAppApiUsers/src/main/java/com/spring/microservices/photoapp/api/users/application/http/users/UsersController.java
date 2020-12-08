f58491c099443596ac9903301242472a6d9f4dd6
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-12-08 16:55:56 -0300

    Add logic to load albums from user if there's a expand query param

--- PhotoAppApiUsers/src/main/java/com/spring/microservices/photoapp/api/users/application/http/users/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/spring/microservices/photoapp/api/users/application/http/users/UsersController.java
@@ -1,5 +1,8 @@
 package com.spring.microservices.photoapp.api.users.application.http.users;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpHeaders;
@@ -10,9 +13,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spring.microservices.photoapp.api.users.application.http.albums.AlbumResponseBody;
 import com.spring.microservices.photoapp.api.users.application.http.exception.ResourceNotFoundException;
+import com.spring.microservices.photoapp.api.users.domain.albums.AlbumDto;
 import com.spring.microservices.photoapp.api.users.domain.users.UserDto;
 import com.spring.microservices.photoapp.api.users.domain.users.UsersService;
 import com.spring.microservices.photoapp.api.users.domain.users.exception.UserNotFoundException;
@@ -41,10 +47,17 @@ public class UsersController {
 	}
 	
 	@GetMapping("{id}")
-	public ResponseEntity<UserResponseBody> getUserById(@PathVariable String id) throws ResourceNotFoundException {
+	public ResponseEntity<UserResponseBody> getUserById(@PathVariable String id, @RequestParam("expand") String expand) throws ResourceNotFoundException {
 		try {
 			UserDto user = userService.getUserById(id);
-			return ResponseEntity.ok(UserResponseBody.of(user));
+			UserResponseBody responseBody = UserResponseBody.of(user);
+			if (expand.toLowerCase().contains("albums")) {
+				List<AlbumDto> albums = userService.getAlbumsByUserId(id);
+				List<AlbumResponseBody> albumsResponseBody = new ArrayList<>();
+				albums.forEach(album -> albumsResponseBody.add(AlbumResponseBody.of(album)));
+				responseBody.setAlbums(albumsResponseBody);
+			}
+			return ResponseEntity.ok(responseBody);
 		} catch (UserNotFoundException err) {
 			throw new ResourceNotFoundException(err.getMessage());
 		}
