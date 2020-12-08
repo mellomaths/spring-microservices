0b3c317398ffb2953e4a23219570d295a37547a8
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-12-08 16:31:45 -0300

    Change mock response on getAlbums service

--- PhotoAppApiAlbums/src/main/java/com/spring/microservices/photoapp/api/albums/domain/albums/AlbumsServiceImpl.java
+++ PhotoAppApiAlbums/src/main/java/com/spring/microservices/photoapp/api/albums/domain/albums/AlbumsServiceImpl.java
@@ -12,8 +12,8 @@ public class AlbumsServiceImpl implements AlbumsService {
 	public List<AlbumDto> getAlbumsByUserId(String userId) {
 		List<Album> albums = new ArrayList<Album>();
 		
-		albums.add(new Album(userId, "My Beautiful Dark Twisted Fantasy", "My Beautiful Dark Twisted Fantasy é o quinto álbum de estúdio pelo rapper, cantor e compositor de hip hop americano Kanye West. Foi primeiramente lançado em 22 de Novembro de 2010 pela editora discográfica Roc-A-Fella Records."));
-		albums.add(new Album(userId, "To Pimp a Butterfly", "To Pimp a Butterfly é o terceiro álbum de estúdio do rapper estadunidense Kendrick Lamar. O álbum foi lançado em 16 de março de 2015, pelas editoras discográficas Top Dawg Entertainment, Aftermath Entertainment, e distribuído pela Interscope Records"));
+		albums.add(new Album(userId, "My Beautiful Dark Twisted Fantasy", "My Beautiful Dark Twisted Fantasy is the fifth studio album by American rapper and producer Kanye West. It was released on November 22, 2010, by Def Jam Recordings and Roc-A-Fella Records."));
+		albums.add(new Album(userId, "To Pimp a Butterfly", "To Pimp a Butterfly is the third studio album by American rapper Kendrick Lamar. It was released on March 15, 2015, by Top Dawg Entertainment, Aftermath Entertainment and Interscope Records."));
 		
 		ArrayList<AlbumDto> result = new ArrayList<AlbumDto>();
 		albums.forEach(album -> result.add(album.toDto())); 
