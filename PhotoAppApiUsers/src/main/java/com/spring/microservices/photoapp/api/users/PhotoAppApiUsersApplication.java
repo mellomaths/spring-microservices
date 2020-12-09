c9e8698ac31f7f9e5b213faf6205a68370a1838e
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-12-09 00:01:26 -0300

    Enable circuit breaker on users microservice

--- PhotoAppApiUsers/src/main/java/com/spring/microservices/photoapp/api/users/PhotoAppApiUsersApplication.java
+++ PhotoAppApiUsers/src/main/java/com/spring/microservices/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -2,12 +2,14 @@ package com.spring.microservices.photoapp.api.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class PhotoAppApiUsersApplication {
 
 	public static void main(String[] args) {
