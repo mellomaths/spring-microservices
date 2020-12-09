8e532246e160c14cd446d1c0f1045f6acf4d7d38
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-12-08 21:39:39 -0300

    Remove unused import

--- PhotoAppApiUsers/src/main/java/com/spring/microservices/photoapp/api/users/application/http/security/AuthenticationFilter.java
+++ PhotoAppApiUsers/src/main/java/com/spring/microservices/photoapp/api/users/application/http/security/AuthenticationFilter.java
@@ -8,7 +8,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
